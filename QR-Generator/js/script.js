52ca49f9310d9eca1fd4a3d9fd0ab4531a091f81
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-06-25 23:31:54 +0530

    init

--- QR-Generator/js/script.js
+++ QR-Generator/js/script.js
@@ -1,20 +1,22 @@
+// Function for QR Code
+var qr;
+(function() {
+        qr = new QRious({
+        element: document.getElementById('qr-code'),
+        size: 250,
+        foreground: '#3B3B98',
+        value: 'https://github.com/joshiujjawal22'
+    });
+})();
 
-			var qr;
-			(function() {
-                    qr = new QRious({
-                    element: document.getElementById('qr-code'),
-                    size: 200,
-                    value: 'https://github.com/joshiujjawal22'
-                });
-            })();
-            
-            function generateQRCode() {
-                var qrtext = document.getElementById("qr-text").value;
-                document.getElementById("qr-result").innerHTML = "QR code for " + qrtext +":";
-                alert(qrtext);
-                qr.set({
-                    foreground: 'black',
-                    size: 200,
-                    value: qrtext
-                });
-            }
+// To generate custom QR COde
+function generateQRCode() {
+    var qrtext = document.getElementById("qr-text").value;
+    document.getElementById("result").innerHTML = "This is QR code for " + qrtext +":";
+    alert(qrtext);
+    // To change the value of QR
+    qr.set({
+        size: 300,
+        value: qrtext
+    });
+}
