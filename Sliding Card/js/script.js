0c238dda46cc448df82c1c406cfb29706028e282
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-09-12 02:22:31 +0530

    Sliding Cards using flickity

---
+++ Sliding Card/js/script.js
@@ -0,0 +1,11 @@
+console.clear();
+
+var flkty = new Flickity('.card-wrap', {
+  contain: true,
+  prevNextButtons: false,
+  pageDots: false,
+  wrapAround: true,
+  adaptiveHeight: true,
+  setGallerySize: false,
+  selectedAttraction: 0.05,
+  freeScrollFriction: .1 });
